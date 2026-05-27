73917b57e6724fcae17c53ac5fc9803a426e2f8e
Victor-droid-ux <onuhvictorali95@gmail.com>
2026-05-27 18:13:21 +0100

    Run standalone Next server in PM2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,8 +2,8 @@ module.exports = {
   apps: [
     {
       name: "blockmec-qr",
-      script: "npm",
-      args: "run start",
+      script: "node",
+      args: ".next/standalone/server.js",
       cwd: "/var/www/Blocmec",
       env: {
         NODE_ENV: "production",
